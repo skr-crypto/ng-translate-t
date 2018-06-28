b19f6f4ed620da3885e35e450b96659a6243be15
Jacob Wejendorp <jwe@tradeshift.com>
2018-06-28 14:00:30 +0200

    test: 100% coverage!

--- src/__tests__/translate.spec.js
+++ src/__tests__/translate.spec.js
@@ -15,19 +15,19 @@ const getValue = el => {
 	return unescaped;
 };
 
-const ref = (text, params, context) => JSON.stringify({ text, params, context })
+const ref = (text, params, context) => JSON.stringify({ text, params, context });
 
 describe('translation directives', () => {
 	let $compile;
 	let $rootScope;
+	let $translate;
 
 	const compileAndDigest = (html, baseScope) => {
 		const scope = $rootScope.$new();
 		Object.assign(scope, baseScope || {});
-
 		const element = $compile(html)(scope);
 		scope.$apply();
-		return element;
+		return { element, scope };
 	};
 
 	beforeEach(() => {
@@ -35,42 +35,58 @@ describe('translation directives', () => {
 		angular.mock.module(translateModule, function($translateProvider) {
 			$translateProvider.setTranslationFunction(ref);
 		});
-		angular.mock.inject(
-			function(_$compile_, _$rootScope_) {
-				$compile = _$compile_;
-				$rootScope = _$rootScope_;
-			}
-		);
+		angular.mock.inject(function(_$compile_, _$rootScope_, _$translate_) {
+			$compile = _$compile_;
+			$rootScope = _$rootScope_;
+			$translate = _$translate_;
+		});
 	});
 
 	describe('t', () => {
 		it('replaces innerHTML with lookup', () => {
-			const element = compileAndDigest('<div t>Open my app</div>');
+			const { element } = compileAndDigest('<div t>Open my app</div>');
 			expect(getValue(element)).toEqual(ref('Open my app', {}));
 		});
 
 		it('replaces innerHTML with lookup and t-context', () => {
-			const element = compileAndDigest('<div t t-context="Hi">Open my app</div>');
+			const { element } = compileAndDigest('<div t t-context="Hi">Open my app</div>');
 			expect(getValue(element)).toEqual(ref('Open my app', {}, 'Hi'));
 		});
 
 		it('handles complex strings w/ refs', () => {
-			const element = compileAndDigest('<div t>Open my app<span id="1">foo</span> </div>');
+			const { element } = compileAndDigest('<div t>Open my app<span id="1">foo</span> </div>');
 			expect(getValue(element)).toEqual(ref('Open my app<span ref="1">foo</span>', {}));
 		});
 
 		it('handles attr params', () => {
-			const element = compileAndDigest('<div t t-param="foo">Open my app</div>', {foo: 'bar'});
-			expect(getValue(element)).toEqual(ref('Open my app', {param: 'bar'}));
+			const { element } = compileAndDigest('<div t t-param="foo">Open my app</div>', { foo: 'bar' });
+			expect(getValue(element)).toEqual(ref('Open my app', { param: 'bar' }));
 		});
 
 		it('handles numeric attr params', () => {
-			const element = compileAndDigest('<div t t-param="foo">Open my app</div>', {foo: '10'});
-			expect(getValue(element)).toEqual(ref('Open my app', {param: 10}));
+			const { element } = compileAndDigest('<div t t-param="foo">Open my app</div>', { foo: '10' });
+			expect(getValue(element)).toEqual(ref('Open my app', { param: 10 }));
+		});
+
+		it('handles undefined attrs', () => {
+			const { element } = compileAndDigest('<div t t-param="foo">Open my app</div>', { });
+			expect(getValue(element)).toEqual(ref('Open my app', { param: '' }));
+		})
+
+		it('handles scope updates', () => {
+			const { element, scope } = compileAndDigest('<div t t-param="foo">Open my app</div>', { foo: 'bar' });
+			expect(getValue(element)).toEqual(ref('Open my app', { param: 'bar' }));
+			scope.foo = 'baz';
+			scope.$apply();
+			expect(getValue(element)).toEqual(ref('Open my app', { param: 'baz' }));
+			scope.foo = undefined;
+			scope.$apply();
+			// undefined becomes empty string:
+			expect(getValue(element)).toEqual(ref('Open my app', { param: '' }));
 		});
 
 		it('replaces inline whitespaces with single whitespace', () => {
-			const element = compileAndDigest(`<p
+			const { element } = compileAndDigest(`<p
 			class="description"
 			t t-context="Messages when no groups available"
 	>You haven't created any groups yet. Groups are used to organise which companies
@@ -85,13 +101,34 @@ describe('translation directives', () => {
 				)
 			);
 		});
+
+		it('gracefully handles double compilation', () => {
+			const { element, scope } = compileAndDigest('<div t t-param="foo">Open my app</div>', { foo: 'bar' });
+			$translate.registerBodyTranslation(scope, element, {});
+			scope.$apply();
+		});
 	});
 
 	describe('t-attrs', () => {
 		it('replaces element attributes', () => {
-			const element = compileAndDigest('<div t-context="context" t-attrs="foo,bar" foo="Hi" bar="Baz"></div>');
+			const { element } = compileAndDigest(
+				'<div t-context="context" t-attrs="foo,bar" foo="Hi" bar="Baz"></div>'
+			);
 			expect(element.attr('foo')).toEqual(ref('Hi', {}, 'context'));
 			expect(element.attr('bar')).toEqual(ref('Baz', {}, 'context'));
+		});
+
+		it('gracefully handles double compilation', () => {
+			const { element, scope } = compileAndDigest('<div t-attrs="foo" foo="hi">Open my app</div>');
+			$translate.registerAttrTranslation(scope, element, {}, 'foo');
+			scope.$apply();
+		});
+	});
+
+	describe('odd stuff', () => {
+		it('leaves gui attr alone', () => {
+			// No idea what this does:
+			compileAndDigest('<div t>Open my app <span gui="wat"></span></div>');
 		});
 	});
 });
